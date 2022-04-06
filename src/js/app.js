9feabf387dfe2127b146e51750c20b9ffef734e5
Tetiana <podleskatetiana@gmail.com>
2022-04-06 20:58:26 +0300

    footer / finished, need to link jquery

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 import * as flsFunctions from './modules/functions.js';
-import Swiper, { Navigation, Pagination } from 'swiper';
 
 flsFunctions.isWebp();
 
-const swiper = new Swiper();
+$(function() {
+    $('.header__btn').on('click', function() {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    })
+    $('.rightside-menu__close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    })
+})
